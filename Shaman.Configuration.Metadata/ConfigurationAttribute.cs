f826003f2f1b71f49df90be73c16b2c77f1e42a9
Andrea Martinelli <martinelli-andrea@outlook.com>
2016-02-08 11:41:33 +0100

    Initial sync with Shaman (ae47bd8c485c)

---
+++ Shaman.Configuration.Metadata/ConfigurationAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Shaman.Runtime
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public class ConfigurationAttribute : Attribute
+    {
+        public string CommandLineAlias { get; set; }
+        public object PerformanceValue
+        {
+            set
+            {
+                _performanceValueSet = true;
+                _performanceValue = value;
+            }
+            get
+            {
+                return _performanceValue;
+            }
+        }
+
+        internal object _performanceValue;
+        internal bool _performanceValueSet;
+        
+        public bool HasPerformanceValue { get { return _performanceValueSet; } }
+    }
+}
